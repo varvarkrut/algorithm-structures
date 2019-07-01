aa95170aa0056d0deaee4a84d0c9915732c5b294
varvarkrut <vanya.shcurko@gmail.com>
2019-07-01 21:51:28 +0300

    Update heap_max.py

--- heap_max.py
+++ heap_max.py
@@ -1,29 +1,25 @@
-#the main idea of this code- you must  add element to a heap  with "-".For that i wrote a function 'invert_list'
-def invert_list(a):
-	for i in range(len(a)):
-		a[i]=a[i]*-1
-
-
-def shuffle(i):
-	while (i>1) and (a[i//2]>a[i]):
+def shake_up(i):
+	while (i>0) and (a[i//2]>a[i]):
 		x=a[i]
 		a[i]=a[i//2]
 		a[i//2]=x
 		i=i//2
 def insert(a,n):
+	n*=-1
 	a.append(n)
-	shuffle(len(a)-1)
+
+	shake_up(len(a)-1)
 def peek_max(a):
 	print('max',abs(a[0]))
 
-def shuffle_down(i):
+def shake_down(i):
 	j=0
 	n=len(a)-1
 	while (2*i<=n):
 		j=i
-		if abs((a[2*i])<abs(a[j])):
+		if (a[2*i])<a[j]:
 			j=2*i
-		if abs((2*i+1<=n)) and abs((a[(2*i)+1]<a[j])):
+		if (2*i+1<=n) and (a[(2*i)+1]<a[j]):
 			j=(2*i)+1
 		if i==j:
 			break
@@ -38,15 +34,19 @@ def ex_max():
 	print('remove max.element')
 	a[0]=a[len(a)-1]
 	a.pop(len(a)-1)
-	shuffle_down(0)
+	shake_down(0)
 
 
 
 
 if __name__=='__main__':
-	a=[5,4,3,2,1]
-	print(a)
-	invert_list(a)
+	a=[]
+	insert(a,200)
+	insert(a,10)
 	peek_max(a)
-	ex_max()
+	insert(a,5)
+	insert(a,500)
+	insert(a,-3)
+	insert(a,100000)
+	print(a)
 	peek_max(a)
