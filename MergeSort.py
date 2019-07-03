9ecc7ef2583d484e3a1bba287bbc3ecc1fc5c254
varvarkrut <vanya.shcurko@gmail.com>
2019-07-03 21:46:26 +0300

    Update MergeSort.py

--- MergeSort.py
+++ MergeSort.py
@@ -1,31 +1,45 @@
-def MergeSort(a):
-    n=len(a)
-    if n<2:
-        return(a)
-        print('hi')
-    m=len(a)//2
-    return Merge(MergeSort(a[0:m]),MergeSort(a[m:]))
+z = 0
 
-def Merge(l,r):
-    res=[]
-    i=0
-    j=0
-    while i<len(l) or j<len(r):
-        if  i == len(l):
+
+def MergeSort(a):
+    n = len(a)
+    if n < 2:
+        return(a)
+    m = len(a) // 2
+    return Merge(MergeSort(a[0:m]), MergeSort(a[m:]))
+
+
+def Merge(l, r):
+    global z
+    res = []
+    i = 0
+    j = 0
+    while i < len(l) or j < len(r):
+        if i == len(l):
             res.append(r[j])
             j += 1
         elif j == len(r):
             res.append(l[i])
             i += 1
-        elif l[i]>r[j]:
+        elif l[i] >= r[j]:
+            if len(l) > 1:
+                if l[i] == r[j] and l[i + 1] > r[j]:
+                    z += (len(l) - (i + 1))
+            if l[i] > r[j]:
+                z += len(l) - i
             res.append(r[j])
-            j=j+1
-        elif r[j]>l[i]:
+            j = j + 1
+        elif r[j] > l[i]:
             res.append(l[i])
-            i=i+1
+            i = i + 1
     return(res)
 
+print('Ввведите числа массива через пробел')
+a = input()
+a = a.split()
 
-a=[-5,3,1,4,8,5,0,-3]
-a=MergeSort(a)
+for i in range(len(a)):
+    a[i] = int(a[i])
+a = MergeSort(a)
 print(a)
+print('количество инверсий в заданном массиве:', z)
