0f950edddc82594fb95bb4e04f5d9d97226ca235
varvarkrut <vanya.shcurko@gmail.com>
2019-07-03 22:57:47 +0300

    Create InserionSort.py

---
+++ InserionSort.py
@@ -0,0 +1,13 @@
+def InsertionSort(a):
+    n=len(a)
+    for i in range(1,n):
+        j=i
+        while j>0 and a[j]<a[j-1]:
+            a[j],a[j-1]=a[j-1],a[j]
+            j=j-1
+            print(a)
+    return(a)
+
+a=[3,2,1,5]
+a=InsertionSort(a)
+print(a)
