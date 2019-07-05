17e36e1ff9bba70554047951e37d0803b13051be
varvarkrut <vanya.shcurko@gmail.com>
2019-07-05 12:48:06 +0300

    Create HeapSort_not_in_place.py

---
+++ HeapSort_not_in_place.py
@@ -0,0 +1,18 @@
+from heapq import heapify,heappop,heappush
+
+
+
+def HeapSort(a):
+    n=len(a)
+    h=[]
+    a1=[]
+    for i in range(0,n):
+        heappush(h,a[i])
+    for i in range(0,n):
+        a1.append(heappop(h))
+    return(a1)
+a=[0,3,5,1,-10,-20]
+
+a=HeapSort(a)
+
+print(a)
