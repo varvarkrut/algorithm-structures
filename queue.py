71d122d0c385fa27c1813106b6130af2196db755
varvarkrut <vanya.shcurko@gmail.com>
2019-07-24 22:57:10 +0300

    Update and rename shit.py to queue.py

--- queue.py
+++ queue.py
@@ -14,7 +14,6 @@ class Stack:
             else:
                 self.items.append([item,self.peek()[1]])
     def maxpush2(self, item):
-        print('item=',item)
         if self.isEmpty()==True:
             self.items.append([item[0],item[0]])
         else:
@@ -34,32 +33,43 @@ class Stack:
     def size(self):
         return len(self.items)
 
-def IsBalanced(c):
+def IsBalanced(c,m):
+    if m==1:
+        for i in range(len(c)):
+            print(c[i],end=" ")
+        return
     a = Stack()
     b = Stack()
     d=[]
-    m=12
     for i in range(0,m-1):
         a.maxpush(c[i])
-    print(a.items)
     for i in range(m-1,len(c)):
         if (b.isEmpty()==True):
             for j in range(m-1):
                 b.maxpush2(a.pop())
-            print(b.items)
         a.maxpush(c[i])
-        print('a[items]=',a.items)
-        print('b[items]=',b.items)
         d.append(max(a.peek()[1],b.peek()[1]))
-        print('d=',d)
         b.pop()
     if len(d)==1:
         print(d[0])
+    else:
+        for i in d:
+            print(i,end=" ")
 
 
 
 
 
 
-c=[28,7,64,40,68,86,80,93,4,53,32,56,68,18,59]
-IsBalanced(c)
+l=input()
+c=input()
+c=c.split(' ')
+for i in range(len(c)):
+    c[i]=int(c[i])
+m=int(input())
+if m==1:
+    for i in range(len(c)):
+        print(c[i],end=" ")
+
+else:
+    IsBalanced(c,m)
