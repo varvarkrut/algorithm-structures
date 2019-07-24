b38e3de73a27897feb4e4a344407bb0e0030e155
varvarkrut <vanya.shcurko@gmail.com>
2019-07-24 09:02:43 +0300

    Create Stack.py

---
+++ Stack.py
@@ -0,0 +1,23 @@
+        return len(self.items)
+
+
+
+def IsBalanced(b):
+    a = Stack()
+    for char in range(len(b)):
+        if b[char] == '[' or b[char] == '(' or b[char] == '{':
+            a.push(char)
+        elif b[char] == ']' or b[char] == ')' or b[char] == '}':
+            if a.isEmpty():
+                return(char+1)
+            top = b[a.pop()]
+            if (top == '[' and b[char] != ']') or (top == '(' and b[char] != ')') or (top == '{' and b[char] != '}'):
+                return(char+1)
+    if a.isEmpty() == True:
+        return('Success')
+    else:
+        return(a.pop()+1)
+
+b=str(input())
+'''b='{[}'''
+print(IsBalanced(b))
