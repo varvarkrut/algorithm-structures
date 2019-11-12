0420141a8ca46157bff9118093679a199c75d7e1
varvarkrut <vanya.shcurko@gmail.com>
2019-11-12 19:08:39 +0300

    Update MergeSort.py

--- MergeSort.py
+++ MergeSort.py
@@ -1,6 +1,3 @@
-z = 0
-
-
 def MergeSort(a):
     n = len(a)
     if n < 2:
@@ -10,36 +7,33 @@ def MergeSort(a):
 
 
 def Merge(l, r):
-    global z
     res = []
     i = 0
     j = 0
-    while i < len(l) or j < len(r):
-        if i == len(l):
+    while (i < len(l) and j < len(r)):
+        if (l[i] > r[j]):
             res.append(r[j])
             j += 1
-        elif j == len(r):
+            continue
+        if (l[i] == r[j]):
+            res.append(l[i])
+            res.append(r[j])
+            j += 1
+            i += 1
+            continue
+        else:
             res.append(l[i])
             i += 1
-        elif l[i] >= r[j]:
-            if len(l) > 1:
-                if l[i] == r[j] and l[i + 1] > r[j]:
-                    z += (len(l) - (i + 1))
-            if l[i] > r[j]:
-                z += len(l) - i
+            continue
+    if (i == len(l)):
+        for j in range(j, len(r), 1): #один из массивов закончился, циклом забираем оставшийся.
             res.append(r[j])
-            j = j + 1
-        elif r[j] > l[i]:
+    else:
+        for i in range(i, len(l), 1):
             res.append(l[i])
-            i = i + 1
     return(res)
 
-print('Ввведите числа массива через пробел')
-a = input()
-a = a.split()
 
-for i in range(len(a)):
-    a[i] = int(a[i])
+a = [10,8,6,2,4,5]
 a = MergeSort(a)
 print(a)
-print('количество инверсий в заданном массиве:', z)
