cfb9cb08d4bcffb88aecba0fda0f3101694f85e2
varvarkrut <vanya.shcurko@gmail.com>
2019-11-19 22:22:27 +0300

    Update QuickSort3.py

--- QuickSort3.py
+++ QuickSort3.py
@@ -1,38 +1,41 @@
 def QuickSort(a, l, r):
     if l >= r:
         return
-    z = Partition(a, l, r)
-    m = z[0]
-    d = z[1]
-    QuickSort(a, l, m)
-    QuickSort(a, d, r)
+    m = Partition(a, l, r)
+    if len(m)==1:
+        QuickSort(a, l, m[0])
+        QuickSort(a, m[0]+1, r)
+    elif len(m)==2:
+        QuickSort(a, l, m[0])
+        QuickSort(a, m[0]+m[1]+1, r)
 
 
 def Partition(a, l, r):
     x = a[l]
     j = l
+    m=0
+    res=[]
     for i in range(l + 1, r):
         if a[i] < x:
             j = j + 1
             a[j], a[i] = a[i], a[j]
+        if a[i]==x:
+            m=j+1
+            a[m],a[i]=a[m],a[i]
     a[l], a[j] = a[j], a[l]
-    '''print('a before',a,'j=',j)'''
-
-    if j < r:
-        k = j
-        for i in range(j + 1, r):
-            if a[i] == a[j]:
-                k = k + 1
-                a[k], a[i] = a[i], a[k]
-        '''print(a)
-        print('j=',j,'k=',k)'''
-        return (j, k + 1)
-
-
+    if m==0:
+        res.append(j)
+        return res
+    else:
+        res.append(j)
+        res.append(m)
+        return res
 
 import time
 start_time = time.time()
-a = [5,0,2,4,6,5,5,-100,-500,500,3000,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5]
+a = [5]*500
+a.append(0)
+a.append(-3)
 QuickSort(a, 0, len(a))
 print(a)
 print("--- %s seconds ---" % (time.time() - start_time))
